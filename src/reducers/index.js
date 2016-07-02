90410822fa9a9a51f87188afe1978eb5263517f0
Mike Ulvila <mikeulvila@me.com>
2016-07-02 10:06:25 -0500

    passing payload request promise through redux-promise middleware to unwrap promise

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import weatherReducer from './reducer_weather';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  weather: weatherReducer
 });
 
 export default rootReducer;
