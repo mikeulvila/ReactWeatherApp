313fe90e96dd95b5ccc1b048519e7d9e4d4e05a6
Mike Ulvila <mikeulvila@me.com>
2016-07-02 20:53:54 -0500

    create weather list container

---
+++ src/containers/weather_list.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import connect from 'react-redux';
+
+class WeatherList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
+    );
+  }
+}
+
+function mapStateToProps({ weather }) {
+  return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,12 @@
-export default function (state=null, action) {
-  console.log('Action received', action);
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function (state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      return [ action.payload.data, ...state ]
+  // same as:
+  //  return state.concat([action.payload.data])
+  }
+
   return state;
 }
